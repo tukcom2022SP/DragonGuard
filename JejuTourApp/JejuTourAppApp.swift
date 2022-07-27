f6a54828982ebe26c4fead434eb1967db4f00690
HJ39 <jeonghojin03@gmail.com>
2022-07-27 23:29:53 +0900

    Initial Commit

---
+++ JejuTourApp/JejuTourAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  JejuTourAppApp.swift
+//  JejuTourApp
+//
+//  Created by 정호진 on 2022/07/27.
+//
+
+import SwiftUI
+
+@main
+struct JejuTourAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
