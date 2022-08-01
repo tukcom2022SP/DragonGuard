406e89ae243612c2d270fd727b96727a5bbfaaf7
HJ39 <jeonghojin03@gmail.com>
2022-08-01 11:51:49 +0900

    Initial Commit

---
+++ montak/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  montak
+//
+//  Created by 정호진 on 2022/08/01.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ montak/montakApp.swift
@@ -0,0 +1,17 @@
+//
+//  montakApp.swift
+//  montak
+//
+//  Created by 정호진 on 2022/08/01.
+//
+
+import SwiftUI
+
+@main
+struct montakApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
