03e597592ff63eb5649fb450779704479559a514
HJ39 <jeonghojin03@gmail.com>
2022-08-01 15:06:10 +0900

    화면 구성 90%완료(Scroll 적용필요)

---
+++ montak/subScreen2.swift
@@ -0,0 +1,28 @@
+//
+//  subScreen2.swift
+//  montak
+//
+//  Created by 정호진 on 2022/08/01.
+//
+
+import SwiftUI
+
+struct subScreen2: View {
+    var body: some View {
+        ZStack{
+            Image("monttakmain")
+                .resizable()
+                .edgesIgnoringSafeArea(.all)
+            
+            VStack{
+                
+            }
+        }//ZStack
+    }
+}
+
+struct subScreen2_Previews: PreviewProvider {
+    static var previews: some View {
+        subScreen2()
+    }
+}
