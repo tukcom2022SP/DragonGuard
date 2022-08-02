112ad85b4d5ab97e0990331905fef8896198d763
posite <su84053@gmail.com>
2022-08-02 14:35:22 +0900

    로딩화면 구현

--- app/src/main/java/com/example/monttak/MainActivity.kt
+++ app/src/main/java/com/example/monttak/MainActivity.kt
@@ -1,12 +1,17 @@
 package com.example.monttak
 
 import android.app.Activity
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+
 
 class MainActivity : Activity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val intent = Intent(this, LoadingActivity::class.java)
+        startActivity(intent)
     }
 }
