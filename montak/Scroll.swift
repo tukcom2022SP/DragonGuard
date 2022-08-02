64ea56236fdeae7120f5f2af877f2dc0b5cff628
HJ39 <jeonghojin03@gmail.com>
2022-08-02 18:04:19 +0900

    JSON데이터 분류 완료, SubScreen2 작업 필요

--- montak/Scroll.swift
+++ montak/Scroll.swift
@@ -18,62 +18,22 @@ struct Scroll: View {
                 ForEach(0..<self.mItem.count){ i in
                     
                     if((self.mItem[i].contentscd.label == "음식점") && (index == 0)){
-                        Button(action: {print("\(i)음식점")} ){
-                            ZStack{
-                                Image("textbar")
-                                    .resizable()
-                                    .frame(width: 230, height: 70)
-                                    .padding(.leading)
-                                Text("\(mItem[i].title)")
-                                    .lineLimit(2)
-                                    .foregroundColor(.black)
-                                    .truncationMode(.middle)
-                            }
-                        }//Button
+                        Scroll_In_Button(mItem: self.mItem, index: i)
                     }
                     else if(self.mItem[i].contentscd.label == "정보" && index == 2){
-                        Button(action: {print("\(i)정보")} ){
-                            ZStack{
-                                Image("textbar")
-                                    .resizable()
-                                    .frame(width: 230, height: 70)
-                                    .padding(.leading)
-                                Text("\(mItem[i].title)")
-                                    .lineLimit(2)
-                                    .foregroundColor(.black)
-                                    .truncationMode(.middle)
-                            }
-                        }//Button
+                        Scroll_In_Button(mItem: self.mItem, index: i)
                     }
                     else if(self.mItem[i].contentscd.label == "숙박" && index == 3){
-                        Button(action: {print("\(i)숙박")} ){
-                            ZStack{
-                                Image("textbar")
-                                    .resizable()
-                                    .frame(width: 230, height: 70)
-                                    .padding(.leading)
-                                Text("\(mItem[i].title)")
-                                    .lineLimit(2)
-                                    .foregroundColor(.black)
-                                    .truncationMode(.middle)
-                            }
-                        }//Button
+                        Scroll_In_Button(mItem: self.mItem, index: i)
                     }
-                    else if (self.mItem[i].contentscd.label == "테마여행" &&
-                             self.mItem[i].contentscd.label == "축제/행사" &&
-                             self.mItem[i].contentscd.label == "쇼핑" && index == 1){
-                        Button(action: {print("\(i)")} ){
-                            ZStack{
-                                Image("textbar")
-                                    .resizable()
-                                    .frame(width: 230, height: 70)
-                                    .padding(.leading)
-                                Text("\(mItem[i].title)")
-                                    .lineLimit(2)
-                                    .foregroundColor(.black)
-                                    .truncationMode(.middle)
-                            }
-                        }//Button
+                    else if (self.mItem[i].contentscd.label == "축제/행사" && index == 1){
+                        Scroll_In_Button(mItem: self.mItem, index: i)
+                    }
+                    else if (self.mItem[i].contentscd.label == "테마여행" && index == 1){
+                        Scroll_In_Button(mItem: self.mItem, index: i)
+                    }
+                    else if (self.mItem[i].contentscd.label == "쇼핑" && index == 1){
+                        Scroll_In_Button(mItem: self.mItem, index: i)
                     }
                     
                 }
