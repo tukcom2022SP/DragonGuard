8184eca5bbd27c00064b2ad5cee058606867f2f5
HJ39 <jeonghojin03@gmail.com>
2022-08-02 14:47:30 +0900

    주석 한 숟갈 추가

--- montak/MainScreen.swift
+++ montak/MainScreen.swift
@@ -4,15 +4,14 @@
 //
 //  Created by 정호진 on 2022/08/01.
 //
-
 import SwiftUI
 
 struct ContentView: View {
     @EnvironmentObject var decodingData: Parsing
     @ObservedObject var value = Parsing()
-    @State var checking : Bool = false
+    @State var checkButtonList : Bool = false
     @State var changeScreen : Bool = false
-    
+    @State var tourTitle : String = ""
     init(){ self.value.getData()}
     
     func timer(){
@@ -20,9 +19,10 @@ struct ContentView: View {
             var mItem: Item
             mItem = self.value.getFirstItem()
             self.changeScreen = true
-            guard let addr = mItem.address else{return}
-            print(mItem.title)
-            print(addr)
+            if let addr = mItem.address {print(addr)}
+            self.tourTitle = mItem.title
+            print("\(mItem.repPhoto)")
+            
         }
     }
     
@@ -37,58 +37,18 @@ struct ContentView: View {
                     Spacer()
                         .frame (height: 50)
                     HStack{
-                        if(checking){
-                            ScrollView{
-                                
-                                VStack {
-                                    ForEach(0..<100){i in
-                                        Image("textbar")
-                                            .resizable()
-                                            .frame(width: 230, height: 30)
-                                            .padding(.leading)
-                                    }
-                                }
-                                .frame(maxWidth:.infinity)
-                            }.frame(width: 230, height: 550)
+                        if(checkButtonList){
+                           Scroll()
+                                .frame(width: 230, height: 550)
                                 .padding(.top)
                         }
                         else{
-                            VStack {
-                                Image("textbar")
-                                    .resizable()
-                                    .frame(width: 230, height: 60)
-                                    .padding(.leading)
-                                Spacer()
-                                    .frame(height:20)
-                                Text("안녕하세요")
-                                    .frame(width: 230, height: 400)
-                                    .padding(.leading)
-                                    .font(.system(size: 20))
-                                
-                            }.frame(width: 230, height: 550)
+                           FirstNotice()
+                                .frame(width: 230, height: 550)
                                 .padding(.top)
-                            
                         }
                         
-                        VStack{
-                            Spacer()
-                                .frame(height:60)
-                            Button(action:{print("Button")}){
-                                Image("mainenroll")
-                                    .resizable()
-                                    .padding(.leading)
-                                    .frame(width: 90, height: 90)
-                            }
-                            Spacer()
-                                .frame(height:330)
-                            Button(action: {print("notice")}){
-                                Image("notice")
-                                    .resizable()
-                                    .padding(.leading)
-                                    .frame(width: 90, height: 90)
-                            }
-                            Spacer()
-                        }
+                        Enroll_Notice()
                         .padding(.leading)
                         .frame(width: 90, height: 610,alignment: .trailing)
                         
@@ -97,22 +57,26 @@ struct ContentView: View {
                     
                     Spacer()
                     HStack{
-                        Button(action: {print("먹거리");self.checking = true}){
+                        Button(action: {
+                            print("먹거리")
+                            self.checkButtonList = true
+                            
+                        }){
                             Image("mainmuk")
                                 .resizable()
                         }
                         Spacer()
-                        Button(action: {print("놀거리"); self.checking = true}){
+                        Button(action: {print("놀거리"); self.checkButtonList = true}){
                             Image("mainnol")
                                 .resizable()
                         }
                         Spacer()
-                        Button(action: {print("볼거리");self.checking = true}){
+                        Button(action: {print("볼거리");self.checkButtonList = true}){
                             Image("mainbol")
                                 .resizable()
                         }
                         Spacer()
-                        Button(action: {print("쉴 곳");self.checking = true}){
+                        Button(action: {print("쉴 곳");self.checkButtonList = true}){
                             Image("mainshuil")
                                 .resizable()
                         }
