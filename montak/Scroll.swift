f182f0c88dd6842af046465cad01acc5272716f0
HJ39 <jeonghojin03@gmail.com>
2022-08-03 15:47:47 +0900

    font 적용, alert dialog 적용 ( 글 수정필요)

--- montak/Scroll.swift
+++ montak/Scroll.swift
@@ -13,32 +13,47 @@ struct Scroll: View {
     
     let field = UITextField()
     var body: some View {
-        ScrollView{
-            VStack {
-                ForEach(0..<self.mItem.count){ i in
-                    if((self.mItem[i].contentscd.label == "음식점") && (index == 0)){
-                        Scroll_In_Button(mItem: self.mItem, index: i)
-                    }
-                    else if(self.mItem[i].contentscd.label == "정보" && index == 2){
-                        Scroll_In_Button(mItem: self.mItem, index: i)
-                    }
-                    else if(self.mItem[i].contentscd.label == "숙박" && index == 3){
-                        Scroll_In_Button(mItem: self.mItem, index: i)
-                    }
-                    else if (self.mItem[i].contentscd.label == "축제/행사" && index == 1){
-                        Scroll_In_Button(mItem: self.mItem, index: i)
-                    }
-                    else if (self.mItem[i].contentscd.label == "테마여행" && index == 1){
-                        Scroll_In_Button(mItem: self.mItem, index: i)
-                    }
-                    else if (self.mItem[i].contentscd.label == "쇼핑" && index == 1){
-                        Scroll_In_Button(mItem: self.mItem, index: i)
-                    }
-                }//ForEach
-            }//VStack
-            .frame(maxWidth:.infinity)
-        }//ScrollView
+        VStack{
+            ZStack {
+                Image("textbar")
+                    .resizable()
+                    .frame(width: 230, height: 70)
+                    .padding(.leading)
+                
+                if(index == 0 ){Text("[먹거리]").font(Font.custom("BinggraeSamanco-Bold",size:30))}
+                else if( index == 1){Text("[놀멍]").font(Font.custom("BinggraeSamanco-Bold", size: 30))}
+                else if( index == 2){Text("[볼거리]").font(Font.custom("BinggraeSamanco-Bold", size: 30))}
+                else if( index == 3){Text("[쉴멍]").font(Font.custom("BinggraeSamanco-Bold", size: 30))}
+                
+            }
+            ScrollView{
+                VStack {
+                    ForEach(0..<self.mItem.count){ i in
+                        if((self.mItem[i].contentscd.label == "음식점") && (index == 0)){
+                            Scroll_In_Button(mItem: self.mItem, index: i)
+                        }
+                        else if(self.mItem[i].contentscd.label == "정보" && index == 2){
+                            Scroll_In_Button(mItem: self.mItem, index: i)
+                        }
+                        else if(self.mItem[i].contentscd.label == "숙박" && index == 3){
+                            Scroll_In_Button(mItem: self.mItem, index: i)
+                        }
+                        else if (self.mItem[i].contentscd.label == "축제/행사" && index == 1){
+                            Scroll_In_Button(mItem: self.mItem, index: i)
+                        }
+                        else if (self.mItem[i].contentscd.label == "테마여행" && index == 1){
+                            Scroll_In_Button(mItem: self.mItem, index: i)
+                        }
+                        else if (self.mItem[i].contentscd.label == "쇼핑" && index == 1){
+                            Scroll_In_Button(mItem: self.mItem, index: i)
+                        }
+                    }//ForEach
+                }//VStack
+                .frame(maxWidth:.infinity)
+            }//ScrollView
+        }
     }
+    
 }
 
 
