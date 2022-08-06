fe51ea138939b689e66269576ba8beebc6e2152c
HJ39 <jeonghojin03@gmail.com>
2022-08-06 22:13:25 +0900

    #작업 카카오 지도 연동 성공
    #해야할 것
    카카오 지도를 크게 생성시 메모리 오류 발생

---
+++ montak/ViewController.swift
@@ -0,0 +1,24 @@
+import UIKit
+
+class ViewController: UIViewController, MTMapViewDelegate {
+ 
+//    var mapView:MTMapView!
+//
+//    override func viewDidLoad() {
+//        super.viewDidLoad()
+//
+//        mapView = MTMapView(frame: self.view.frame)
+//        mapView.delegate = self
+//        mapView.baseMapType = .standard
+//        self.view.addSubview(mapView)
+//
+//        let item1 = MTMapPOIItem()
+//
+//        item1.itemName = "맛집"
+//        item1.mapPoint  = MTMapPoint(geoCoord: MTMapPointGeo(latitude: 3.74, longitude: 126.4))
+//        item1.markerType = .redPin
+//
+//        mapView.addPOIItems([item1])
+//    }
+    
+}
