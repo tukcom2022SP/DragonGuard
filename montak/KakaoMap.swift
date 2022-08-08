a22f3abf17bd478f6a74326f2d2cbefc1197601e
HJ39 <jeonghojin03@gmail.com>
2022-08-09 00:53:26 +0900

    #작업 카카오맵 위치 이동 및 0과 nil값 처리 완료 #작업 해야할 것 updateView 사용하여 마커찍기

--- montak/KakaoMap.swift
+++ montak/KakaoMap.swift
@@ -6,19 +6,50 @@
 //
 
 import SwiftUI
+import MapKit
 
 struct KakaoMap: UIViewRepresentable {
-    
+    var la : Double?
+    var long: Double?
     
     func makeUIView(context: Context) -> MTMapView {
         print("맵 생성")
+        print("la = \(la), long = \(long)")
+        let view = MTMapView(frame: .zero)
+        view.baseMapType = .standard
         
-        return MTMapView()
+//        view.setZoomLevel(1, animated: false)
+//        view.setMapCenter(MTMapPoint(geoCoord: MTMapPointGeo(latitude: la ?? 0, longitude: long ?? 0)), animated: true)
+//
+//        let poiItem = MTMapPOIItem()
+//        poiItem.itemName = "1"
+//        poiItem.mapPoint = MTMapPoint(geoCoord: MTMapPointGeo(latitude: 33.462147, longitude: 126.936424  ))
+//        poiItem.markerType = .redPin
+//        view.addPOIItems([poiItem])
+        
+        return view
+        
     }
     
-    func updateUIView(_ uiView: MTMapView, context: Context) {
+    func updateUIView(_ uiView: MTMapView, context: UIViewRepresentableContext<KakaoMap>) {
+        let view = MTMapView(frame: .zero)
+        view.baseMapType = .standard
+        
+        view.setZoomLevel(1, animated: true)
+        view.setMapCenter(MTMapPoint(geoCoord: MTMapPointGeo(latitude: la ?? 0, longitude: long ?? 0)), animated: true)
+        
+        let poiItem = MTMapPOIItem()
+        poiItem.itemName = "1"
+        poiItem.mapPoint = MTMapPoint(geoCoord: MTMapPointGeo(latitude: la ?? 0, longitude: long ?? 0  ))
+        poiItem.markerType = .redPin
+        view.addPOIItems([poiItem])
         print("업데이트완료")
+        
     }
     
+    func makeCoordinator() -> Coordinate {
+        return Coordinate()
+    }
+    class Coordinate  {}
 }
 
